93222603db18431c35d32be7bbf2193c74967e62
Naji Naji <nnaji@sqli.com>
2020-10-19 12:11:28 +0100

    Set up logs config

--- Mint/Program.cs
+++ Mint/Program.cs
@@ -38,6 +38,12 @@ namespace Mint
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
