dc5f90bbcf3813ac18f84dcd250d9bd738583998
Naji Naji <nnaji@sqli.com>
2020-10-21 15:03:12 +0100

    Use environment varriables for baseUrl

--- client/src/app/services/machine.service.ts
+++ client/src/app/services/machine.service.ts
@@ -2,27 +2,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Machine } from '../models/machine.model';
+import {environment} from "../../environments/environment";
 
 @Injectable()
 export class MachineService {
 
-    baseUrl : string = "https://localhost:44391";
-
     constructor(private http : HttpClient){}
 
     getMachines() {
-        return this.http.get<Machine[]>(`${this.baseUrl}/Machines`);
+        return this.http.get<Machine[]>(`${environment.baseUrl}/Machines`);
     }
 
     getMachine(machineId : number) {
-        return this.http.get<Machine>(`${this.baseUrl}/Machine/${machineId}`);
+        return this.http.get<Machine>(`${environment.baseUrl}/Machine/${machineId}`);
     }
 
     getTotalProductions(machineId : number) {
-        return this.http.get<any>(`${this.baseUrl}/Machine/totalproduction?id=${machineId}`);
+        return this.http.get<any>(`${environment.baseUrl}/Machine/totalproduction?id=${machineId}`);
     }
 
     deleteMachine(machineId : number) {
-        return this.http.delete<number>(`${this.baseUrl}/Machine/${machineId}`);
+        return this.http.delete<number>(`${environment.baseUrl}/Machine/${machineId}`);
     }
 }
