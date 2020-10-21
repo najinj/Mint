423260d71d6f9b7e3885f21eb29d9f728a3a501e
Naji Naji <nnaji@sqli.com>
2020-10-21 15:02:51 +0100

    Refactor code Enhance error handeling

--- client/src/app/machine/machine.component.ts
+++ client/src/app/machine/machine.component.ts
@@ -1,7 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { interval } from 'rxjs';
 import { Machine } from '../models/machine.model';
 import { MachineService } from '../services/machine.service';
+
 
 @Component({
   selector: 'app-machine',
@@ -11,20 +14,46 @@ import { MachineService } from '../services/machine.service';
 export class MachineComponent implements OnInit {
 
   machine : Machine;
-  
+  error : HttpErrorResponse  = null;
 
   constructor(private machineService : MachineService,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    
     const machineId = this.route.snapshot.params['id'];
     this.machine = new Machine(machineId);
+    this.loadMachine();
+
+    const myInterval = interval(5000);
+    myInterval.subscribe(
+      (value) => {
+        this.loadMachine();
+      },
+      (err) => {
+        this.error = err;
+        console.log('Uh-oh, an error occurred! : ' + err);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
+    
+  }
+
+  loadMachine () {
     this.machineService.getMachine(this.machine.machineId).subscribe(( data : Machine) =>{
       this.machine.name = data.name;
       this.machine.description = data.description;
+      this.error = null;
+    },
+    (err : HttpErrorResponse )=>{
+      this.error = err;
     });
     this.machineService.getTotalProductions(this.machine.machineId).subscribe(( data ) =>{
       this.machine.totalproduction = data.totalProdution;
+      this.error = null;
+    },
+    (err : HttpErrorResponse )=>{
+      this.error = err;
     });
   }
 }
